d119dfe3cfd529dd80ace11b6098b70c92b73974
OneCDOnly <one.cd.only@gmail.com>
2020-08-25 05:23:58 +1000

    add new dependency

--- __sherpa-main__.sh
+++ __sherpa-main__.sh
@@ -38,7 +38,7 @@ Init()
     {
 
     readonly SCRIPT_NAME=sherpa.sh
-    readonly SCRIPT_VERSION=200824f
+    readonly SCRIPT_VERSION=200825
 
     IsQNAP || return 1
     IsOnlyInstance || return 1
@@ -230,7 +230,7 @@ Init()
         SHERPA_QPKG_ABBRVS+=('sc sick sickc chill sickchill')
         SHERPA_QPKG_DEPS+=('Entware')
         SHERPA_QPKG_IPKGS+=('python3')
-        SHERPA_QPKG_PIPS+=('random_user_agent pygithub')
+        SHERPA_QPKG_PIPS+=('random_user_agent pygithub slugify')
         #SHERPA_QPKG_PIPS+=('backports-abc beautifulsoup4 beekeeper bencodepy cachecontrol chardet cloudscraper configobj deluge-client enzyme feedparser gntp html5lib httplib2 ifaddr imagesize imdbpie ipaddress js2py jsonrpclib-pelix kodipydent mako markdown2 new-rtorrent-python packaging profilehooks putio.py pyopenssL pyaes pygithub pymediainfo pynma pysocks python-dateutil python-twitter python3-fanart qbittorrent-api random_user_agent rarfile send2trash subliminal timeago tmdbsimple tornado tus.py tvdbsimple twilio unidecode validators')
 
     SHERPA_QPKG_NAME+=(LazyLibrarian)
