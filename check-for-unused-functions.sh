020c74926069727bef5d9be6fa3bf482932e16e5
OneCDOnly <one.cd.only@gmail.com>
2020-10-07 14:18:48 +1000

    reduce variables

--- check-for-unused-functions.sh
+++ check-for-unused-functions.sh
@@ -2,9 +2,7 @@
 
 target_pathfile=sherpa.manager.sh
 target_func=''
-count=0
 
 for target_func in $(grep '()$' "$target_pathfile" | grep -v '=\|\$' | sed 's|()||g'); do
-	count=$(grep -ow "$target_func" < "$target_pathfile" | wc -l)
-	[[ $count -eq 1 ]] && echo "$target_func()"
+    [[ $(grep -ow "$target_func" < "$target_pathfile" | wc -l) -eq 1 ]] && echo "$target_func()"
 done
