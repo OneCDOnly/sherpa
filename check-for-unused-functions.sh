1c9bce2ca50fa9bc8ccee6c8fbff3a87d62bf59d
OneCDOnly <one.cd.only@gmail.com>
2020-10-08 13:45:27 +1000

    disable unwanted error pickup

--- check-for-unused-functions.sh
+++ check-for-unused-functions.sh
@@ -3,6 +3,7 @@
 target_pathfile=sherpa.manager.sh
 target_func=''
 
+# shellcheck disable=SC2013
 for target_func in $(grep '()$' "$target_pathfile" | grep -v '=\|\$' | sed 's|()||g'); do
     [[ $(grep -ow "$target_func" < "$target_pathfile" | wc -l) -eq 1 ]] && echo "$target_func()"
 done
