bd80563ac75f30cd955bca77f97211c9245a67b4
OneCDOnly <one.cd.only@gmail.com>
2020-11-24 08:57:36 +1000

    don't show stalled time in colour as display becomes corrupt - still haven't figured out why ANSI byte-length isn't being calc'd correctly at runtime (works fine in all tests), so disable this for-now, - more quote fixes, - dump some 'echo' calls and use 'Display' & 'DisplayWait' functions instead.

--- check-for-unused-functions.sh
+++ check-for-unused-functions.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "checking ... "
+
 target_pathfile=sherpa.manager.sh
 target_func=''
 
@@ -7,3 +9,5 @@ target_func=''
 for target_func in $(grep '()$' "$target_pathfile" | grep -v '=\|\$' | sed 's|()||g'); do
     [[ $(grep -ow "$target_func" < "$target_pathfile" | wc -l) -eq 1 ]] && echo "$target_func()"
 done
+
+echo "done!"
