63a86a3ba0a8ec8d5ccb16c236db15ef43417f72
OneCDOnly <one.cd.only@gmail.com>
2020-11-25 09:14:46 +1000

    use consistent output between check scripts

--- check-with-shellcheck.sh
+++ check-with-shellcheck.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+fail=false
+
 echo -n "checking ... "
 
-shellcheck --shell=bash --exclude=1090,1117,2016,2018,2019,2086,2155,2181,2206,2207 ./*.sh && echo 'passed!' || echo 'failed!'
+if ! shellcheck --shell=bash --exclude=1090,1117,2016,2018,2019,2086,2155,2181,2206,2207 ./*.sh; then
+    fail=true
+    echo
+fi
+
+[[ $fail = true ]] && echo "failed!" || echo "passed!"
