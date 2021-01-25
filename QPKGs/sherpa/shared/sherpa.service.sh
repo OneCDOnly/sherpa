39af09cbaff57c0be91af0a12eaf4fd3ea8b030a
OneCDOnly <one.cd.only@gmail.com>
2021-01-25 14:41:51 +1000

    update path to new archive debug log location

--- QPKGs/sherpa/shared/sherpa.service.sh
+++ QPKGs/sherpa/shared/sherpa.service.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# sherpa.service.sh - (C)opyright 2020 OneCD [one.cd.only@gmail.com]
+# sherpa.service.sh - (C)opyright 2020-2021 OneCD [one.cd.only@gmail.com]
 #
 # This is the service script for the sherpa mini-package-manager and is part of the 'sherpa' QPKG.
 #
@@ -8,6 +8,7 @@
 #
 # Tested on:
 #  GNU bash, version 3.2.57(2)-release (i686-pc-linux-gnu)
+#  GNU bash, version 3.2.57(1)-release (aarch64-QNAP-linux-gnu)
 #  Copyright (C) 2007 Free Software Foundation, Inc.
 #
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -17,10 +18,10 @@
 # You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
 
 readonly PROJECT_NAME=sherpa
-readonly QPKG_PATH=$(/sbin/getcfg $PROJECT_NAME Install_Path -f /etc/config/qpkg.conf)
-readonly REAL_LOG_PATHFILE=$QPKG_PATH/$PROJECT_NAME.debug.log
+readonly PROJECT_PATH=$(/sbin/getcfg $PROJECT_NAME Install_Path -f /etc/config/qpkg.conf)
+readonly REAL_LOG_PATHFILE=$PROJECT_PATH/logs/session.archive.log
 readonly GUI_LOG_PATHFILE=/home/httpd/$PROJECT_NAME.debug.log
-readonly REAL_LOADER_SCRIPT_PATHNAME=$QPKG_PATH/$PROJECT_NAME.loader.sh
+readonly REAL_LOADER_SCRIPT_PATHNAME=$PROJECT_PATH/$PROJECT_NAME.loader.sh
 readonly APPARENT_LOADER_SCRIPT_PATHNAME=/usr/sbin/$PROJECT_NAME
 
 [[ ! -e $REAL_LOG_PATHFILE ]] && /bin/touch "$REAL_LOG_PATHFILE"
