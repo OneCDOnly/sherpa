9161de86827f77364e55f95017e61f33623b5d12
OneCDOnly <one.cd.only@gmail.com>
2021-04-11 07:33:32 +1000

    ensure 'cryptography' PIP is reinstalled - this is needed if the 'python3-cryptography' IPKG has been installed. Rather use the PIP version but the IPKG version is a dependency for various other IPKGS. Example: 'deluge-web' won't start if 'python3-cryptography' IPKG is installed after 'cryptography' PIP.

--- sherpa.manager.sh
+++ sherpa.manager.sh
@@ -1189,7 +1189,7 @@ Tiers.Processor()
 
                 for operation in Install Reinstall Upgrade; do
                     if QPKGs.OpTo${operation}.Exist SABnzbd; then
-                        PIPs.ToInstall.Set   # must ensure 'sabyenc' and 'feedparser' modules are installed/updated
+                        PIPs.ToInstall.Set   # must ensure 'feedparser' and 'cryptography' modules are installed/updated
                         break
                     fi
                 done
@@ -2535,9 +2535,8 @@ PIPs.DoInstall()
     fi
 
     if QPKGs.OpToInstall.Exist SABnzbd || QPKGs.OpToReinstall.Exist SABnzbd || (Opts.Deps.Check.IsSet && QPKGs.IsInstalled.Exist SABnzbd); then
-        ((total_count+=1))
-
 #         # KLUDGE: force recompilation of 'sabyenc3' package so it's recognised by SABnzbd: https://forums.sabnzbd.org/viewtopic.php?p=121214#p121214
+#         ((total_count+=1))
 #         ShowAsOperationProgress '' "$PACKAGE_TYPE" "$pass_count" "$fail_count" "$total_count" "$ACTION_PRESENT" "$RUNTIME"
 #
 #         exec_cmd="$pip3_cmd install --force-reinstall --ignore-installed --no-binary :all: sabyenc3 --cache-dir $PIP_CACHE_PATH"
@@ -2558,6 +2557,7 @@ PIPs.DoInstall()
 #         fi
 
         # KLUDGE: ensure 'feedparser' is upgraded. This was version-held at 5.2.1 for Python 3.8.5 but from Python 3.9.0 onward there's no-need for version-hold anymore.
+        ((total_count+=1))
         ShowAsOperationProgress '' "$PACKAGE_TYPE" "$pass_count" "$fail_count" "$total_count" "$ACTION_PRESENT" "$RUNTIME"
 
         exec_cmd="$pip3_cmd install --upgrade feedparser --cache-dir $PIP_CACHE_PATH"
@@ -2573,6 +2573,29 @@ PIPs.DoInstall()
             ((pass_count++))
         else
             ShowAsFail "download & install $desc failed $(FormatAsResult "$result_code")"
+            ((fail_count++))
+        fi
+    fi
+
+    if IPKGs.OpToInstall.Exist python3-cryptography || Opts.Deps.Check.IsSet; then
+        # KLUDGE: must ensure 'cryptography' PIP module is reinstalled if the 'python3-cryptography' IPKG is installed.
+        # The 'deluge-ui-web' IPKG pulls-in 'python3-cryptography' IPKG as a dependency, but this then causes a launch-failure for 'deluge-web' due to there already being a later 'cryptography' installed via 'pip'. Prefer to use the 'pip' version, so need to reinstall it so it is seen first.
+        ((total_count+=1))
+        ShowAsOperationProgress '' "$PACKAGE_TYPE" "$pass_count" "$fail_count" "$total_count" "$ACTION_PRESENT" "$RUNTIME"
+
+        exec_cmd="$pip3_cmd install --upgrade cryptography --force-reinstall --cache-dir $PIP_CACHE_PATH"
+        desc="'Python3 cryptography' module"
+        log_pathfile=$LOGS_PATH/py3-modules.cryptography.$REINSTALL_LOG_FILE
+        DebugAsProc "reinstalling $desc"
+        RunAndLog "$exec_cmd" "$log_pathfile" log:failure-only
+        result_code=$?
+
+        if [[ $result_code -eq 0 ]]; then
+            DebugAsDone "reinstalled $desc"
+            QPKGs.OpToRestart.Add SABnzbd
+            ((pass_count++))
+        else
+            ShowAsFail "reinstallation of $desc failed $(FormatAsResult "$result_code")"
             ((fail_count++))
         fi
     fi
