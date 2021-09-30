11ab0e1c70de20618b845ecaef8865b848642699
OneCDOnly <one.cd.only@gmail.com>
2021-10-01 04:59:22 +1000

    use shorter MakePath checks

--- check-with-shellcheck.sh
+++ check-with-shellcheck.sh
@@ -4,7 +4,7 @@ echo -n 'checking ... '
 
 fail=false
 
-if ! shellcheck --shell=bash --exclude=1090,1117,2015,2016,2018,2019,2086,2155,2181,2206,2207 ./*.sh; then
+if ! shellcheck --shell=bash --exclude=1090,1117,2015,2016,2018,2019,2034,2086,2155,2181,2206,2207 ./*.sh; then
     fail=true
     echo
 fi
