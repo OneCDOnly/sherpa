68a844943e9f8bd561b511e87398b76dbfe2cacc
OneCDOnly <one.cd.only@gmail.com>
2021-12-07 07:39:10 +1000

    version bump for SortMyQPKGs

--- sherpa.manager.sh
+++ sherpa.manager.sh
@@ -54,7 +54,7 @@ Session.Init()
     export LC_CTYPE=C
 
     readonly PROJECT_NAME=sherpa
-    local -r SCRIPT_VERSION=211127
+    local -r SCRIPT_VERSION=211207
     readonly PROJECT_BRANCH=main
 
     ClaimLockFile /var/run/$PROJECT_NAME.loader.sh.pid || return
@@ -689,9 +689,9 @@ Session.Init()
     MANAGER_QPKG_NAME+=(SortMyQPKGs)
         MANAGER_QPKG_ARCH+=(all)
         MANAGER_QPKG_MIN_RAM_KB+=(any)
-        MANAGER_QPKG_VERSION+=(211030)
+        MANAGER_QPKG_VERSION+=(211207)
         MANAGER_QPKG_URL+=(https://raw.githubusercontent.com/OneCDOnly/${MANAGER_QPKG_NAME[${#MANAGER_QPKG_NAME[@]}-1]}/main/build/${MANAGER_QPKG_NAME[${#MANAGER_QPKG_NAME[@]}-1]}_${MANAGER_QPKG_VERSION[${#MANAGER_QPKG_VERSION[@]}-1]}.qpkg)
-        MANAGER_QPKG_MD5+=(595a15bc80d5c7cada12c202ab3cc718)
+        MANAGER_QPKG_MD5+=(847f2bd7f3583676de6f431bbb657bf9)
         MANAGER_QPKG_DESC+=('ensure other installed QPKGs start in correct sequence during QTS bootup')
         MANAGER_QPKG_ABBRVS+=('sm smq smqs sort sortmy sortmine sortpackages sortmypackages sortmyqpkgs')
         MANAGER_QPKG_DEPENDS_ON+=('')
