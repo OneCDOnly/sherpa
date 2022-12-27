fea10098c159c8ab05042ac4d8a4d96ebbaa05b7
OneCDOnly <one.cd.only@gmail.com>
2022-12-27 10:37:43 +1000

    include early effort to automate package list rewriter

---
+++ build-packages.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# construct a list of all QPKG checksum files
+find ~/scripts/nas -name '*.qpkg.md5' > /tmp/raw.md5s
+
+# sort this list, grouped by package names, most-recent package name version at the bottom of each group.
+sort -V < /tmp/raw.md5s > /tmp/sorted.md5s
+rm -r /tmp/raw.md5s
+
+checksum_pathfilename=''
+previous_package_name=''
+previous_package_filename=''
+previous_package_pathfilename=''
+package_version=''
+previous_package_version=''
+
+while read -r checksum_pathfilename; do
+    # need just filename
+    filename=$(basename "$checksum_pathfilename")
+
+    # then extract package name (everything from start of filename to first underscore will-do)
+    package_name=${filename%%_*}
+
+    # also need to extract package version here
+    a=${filename#*_}
+    b=${a%%_*}
+    package_version=${b//.qpkg.md5/}
+
+    # and package arch here
+
+    if [[ $package_name != $previous_package_name ]]; then
+        if [[ -n $previous_package_filename ]]; then
+            echo "current: $previous_package_pathfilename ($previous_package_version)"
+            displayed=true
+        fi
+    fi
+
+    previous_package_name=$package_name
+    previous_package_filename=$filename
+    previous_package_pathfilename=$checksum_pathfilename
+    previous_package_version=$package_version
+    displayed=false
+done < /tmp/sorted.md5s
+
+[[ $displayed = false ]] && echo "current: $previous_package_pathfilename ($previous_package_version)"
