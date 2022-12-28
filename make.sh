8039cc87d0521e332d9d5d6167f5753a8679ceb1
OneCDOnly <one.cd.only@gmail.com>
2022-12-29 05:49:15 +1000

    use shorter function names

--- make.sh
+++ make.sh
@@ -10,7 +10,7 @@ WORK_PATH=$PWD
 
 MANAGEMENT_ACTIONS=(Check List Paste Status)
 
-PACKAGE_SCOPES=(All Dependent HasDependents Installable Standalone SupportBackup SupportUpdateOnRestart Upgradable)
+PACKAGE_SCOPES=(All Dependent HasDependents Installable Standalone CanBackup CanRestartToUpdate Upgradable)
 PACKAGE_STATES=(BackedUp Cleaned Downloaded Enabled Installed Missing Started)
 PACKAGE_STATES_TEMPORARY=(Starting Stopping Restarting)
 PACKAGE_ACTIONS=(Backup Clean Disable Download Enable Install Reassign Rebuild Reinstall Restart Restore Start Stop Uninstall Upgrade)
