377e305fb05bab93420ed57319483ca9343eb065
OneCDOnly <one.cd.only@gmail.com>
2023-01-14 13:52:09 +1000

    validate package states found in message stream

--- tests/pipe-reader-poc.sh
+++ tests/pipe-reader-poc.sh
@@ -5,25 +5,29 @@
 # Have multiple background procs all send data to a single named pipe.
 # Then read from this pipe, and update the state of specific QPKG arrays according to details in data.
 
-_Output_()
+_QPKG.Action_()
     {
 
     # * this function runs as a background process *
 
     # $1 = package name
     # $2 = sleep time in decimal seconds
+    # $3 = (optional) forced status to return
 
     [[ -z ${1:?package name null} ]] && exit
     export PACKAGE_NAME=$1
     local -i x=0
 
+    # generate a few messages to test message stream
     for x in {1..8}; do
-        SendPackageChangeStateRequest installed
-        SendPackageChangeStateRequest started
+        SendPackageChangeStateRequest IsInstalled
+        SendPackageChangeStateRequest IsStarted
         sleep $2
     done
 
-    SendProcStatus ok
+    SendPackageChangeStateRequest IsBollocksed      # add something to be picked up as `unidentified`
+
+    SendProcStatus "${3:-ok}"
     SendProcStatus exit
 
     }
@@ -32,8 +36,8 @@ SendPackageChangeStateRequest()
     {
 
     # Send a message into message stream to change the state of this QPKG to $1
-    # This might be: `installed`, `enabled`, `started`, etc...
-    # This function is only called from background functions
+    # This might be: `IsInstalled`, `IsNtEnabled`, `IsStarted`, etc...
+    # This function is only called from within background functions
 
     # $1 = action request
 
@@ -44,9 +48,9 @@ SendPackageChangeStateRequest()
 SendProcStatus()
     {
 
-    # Send a message into message stream to change the status of this action
-    # This might be: `ok`, `skipped`, `failed`, `exit`
-    # This function is only called from background functions
+    # Send a message into message stream to update parent with the status of this action
+    # This can be: `ok`, `skipped`, `failed`, `exit`
+    # This function is only called from within background functions
 
     # $1 = status update
 
@@ -85,29 +89,33 @@ eval "exec $fd<>$message_pipe"
 # launch forks with delays in-between to simulate QPKG actions
 echo "$(now): launch forks"
 packages+=(SABnzbd)
-_Output_ ${packages[${#packages[@]}-1]} 1.3 &
+_QPKG.Action_ ${packages[${#packages[@]}-1]} 1.3 &
 
 sleep 2
 
 packages+=(NZBGet)
-_Output_ ${packages[${#packages[@]}-1]} 2 &
+_QPKG.Action_ ${packages[${#packages[@]}-1]} 2 skipped &
 
 sleep 4
 
 packages+=(HideThatBanner)
-_Output_ ${packages[${#packages[@]}-1]} 1.6 &
+_QPKG.Action_ ${packages[${#packages[@]}-1]} 1.6 failed &
 
 sleep 1
 
 packages+=(SortMyQPKGs)
-_Output_ ${packages[${#packages[@]}-1]} .7 &
+_QPKG.Action_ ${packages[${#packages[@]}-1]} .7 &
 
 echo "$(now): sleep for a bit"
-sleep 2
+sleep 3
 
 passes=0
 skips=0
 fails=0
+
+PACKAGE_STATES=(BackedUp Cleaned Downloaded Enabled Installed Missing Started Upgradable)           # sorted
+
+
 
 echo "$(now): begin processing message stream"
 echo '-----------------------------------------'
@@ -115,7 +123,7 @@ echo '-----------------------------------------'
 while [[ ${#packages[@]} -gt 0 ]]; do
     read input
 
-    # extract 3 values from message stream: package name, state or status, and datetime
+    # whittle-down $input to extract 3 values: package name, state or status, and datetime
 #     package_key="${input%%:*}"
     input="${input#*[}"
 
@@ -135,47 +143,48 @@ while [[ ${#packages[@]} -gt 0 ]]; do
 
     case $state_status_key in
         state)
-            case $state_status_value in
-                installed)
-                    # mark QPKG state as installed
-                    echo "$datetime_value: marking $package_name state as $state_status_value"
-                    ;;
-                started)
-                    # mark QPKG state as started
-                    echo "$datetime_value: marking $package_name state as $state_status_value"
-                    ;;
-                *)
-                    echo "unknown state: <$state_status_value>"
-            esac
+            # must validate the content of $state_status_value before calling it
+            while true; do
+                for state in "${PACKAGE_STATES[@]}"; do
+                    if [[ $state_status_value = "Is${state}" || $state_status_value = "IsNt${state}" ]]; then
+                        echo "$datetime_value: marking $package_name state as $state_status_value"
+#                       NoteQpkgStateAs${state_status_value} "$package_name"
+                        break 2
+                    fi
+                done
+
+                echo "$datetime_value: ignoring unidentified $package_name state: '$state_status_value'"
+                break
+            done
             ;;
         status)
             case $state_status_value in
                 ok)
                     # mark QPKG action as finished OK
-                    echo "$datetime_value: marking $package_name action as $state_status_value"
+                    echo "$datetime_value: marking $package_name action status as $state_status_value"
                     ((passes++))
                     ;;
                 skipped)
                     # mark QPKG action as skipped
-                    echo "$datetime_value: marking $package_name action as $state_status_value"
+                    echo "$datetime_value: marking $package_name action status as $state_status_value"
                     ((skips++))
                     ;;
                 failed)
                     # mark QPKG action as failed
-                    echo "$datetime_value: marking $package_name action as $state_status_value"
+                    echo "$datetime_value: marking $package_name action status as $state_status_value"
                     ((fails++))
                     ;;
                 exit)
                     for index in "${!packages[@]}"; do
                         if [[ ${packages[index]} = "$package_name" ]]; then
-                            echo "$datetime_value: $package_name is complete"
+                            echo "$datetime_value: $package_name action fork is exiting"
                             unset 'packages[index]'
                             break
                         fi
                     done
                     ;;
                 *)
-                echo "unknown status: <$state_status_value>"
+                    echo "$datetime_value: ignoring unidentified $package_name status: '$state_status_value'"
             esac
     esac
 done <&$fd
