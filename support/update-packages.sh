919f242641d4ef80bcb04ff38d57c3cf3c3034d3
OneCDOnly <one.cd.only@gmail.com>
2023-02-05 19:11:20 +1000

    fixed latest package version find

--- support/update-packages.sh
+++ support/update-packages.sh
@@ -4,20 +4,19 @@
 echo 'locating checksum files ...'
 find ~/scripts/nas -name '*.qpkg.md5' > /tmp/raw.md5s
 
-# sort this list, grouped by package names, most-recent package name version at the bottom of each group.
+# sort this list, grouped by package names, most-recent package name version at the top of each group.
 echo 'sorting file list ...'
-sort -V < /tmp/raw.md5s > /tmp/sorted.md5s          # try sorting in reverse order. Then latest version will be first. Should be easier to read other arches with same version.
-rm -r /tmp/raw.md5s
+sort --version-sort --reverse < /tmp/raw.md5s > /tmp/sorted.md5s
+rm --recursive /tmp/raw.md5s
 
 checksum_pathfilename=''
-package_version=''
+checksum_version=''
 checksum_arch=''
-previous_checksum_name=''
-previous_checksum_version=''
 previous_checksum_filename=''
-previous_checksum_pathfilename=''
+previous_package_name=''
+previous_checksum_version=''
 previous_checksum_arch=''
-previous_checksum_md5=''
+match=false
 
 echo '---------- results -----------'
 echo 'highest package version found:'
@@ -27,27 +26,30 @@ while read -r checksum_pathfilename; do
     # need just filename
     checksum_filename=$(basename "$checksum_pathfilename")
 
-    IFS='_' read -r package_name package_version checksum_arch tailend <<< "${checksum_filename//.qpkg.md5/}"
+    IFS='_' read -r package_name checksum_version checksum_arch tailend <<< "${checksum_filename//.qpkg.md5/}"
 
     [[ -n $tailend ]] && checksum_arch+=_$tailend
     [[ -z $checksum_arch ]] && checksum_arch=all
+    checksum_md5=$(cut -f1 -d' ' <$checksum_pathfilename)
 
-    if [[ $package_name != "$previous_checksum_name" ]]; then
-        if [[ -n $previous_checksum_filename ]]; then
-            printf '%-36s %-14s %-10s %s\n' "$previous_checksum_filename" "$previous_checksum_version" "$previous_checksum_arch" "$previous_checksum_md5"
-            displayed=true
+    if [[ $package_name != "$previous_package_name" ]]; then
+        match=true
+    elif [[ $checksum_version = $previous_checksum_version ]]; then
+        if [[ $checksum_arch != $previous_checksum_arch ]]; then
+            match=true
         fi
+    else
+        match=false
     fi
 
-    previous_checksum_name=$package_name
-    previous_checksum_filename=$checksum_filename
-    previous_checksum_pathfilename=$checksum_pathfilename
-    previous_checksum_version=$package_version
-    previous_checksum_arch=$checksum_arch
-    previous_checksum_md5=$(cut -f1 -d' ' <$checksum_pathfilename)
-    displayed=false
+    if [[ $match = true ]]; then
+        printf '%-36s %-14s %-10s %s\n' "$checksum_filename" "$checksum_version" "$checksum_arch" "$checksum_md5"
+        previous_checksum_filename=$checksum_filename
+        previous_package_name=$package_name
+        previous_checksum_version=$checksum_version
+        previous_checksum_arch=$checksum_arch
+    fi
 done < /tmp/sorted.md5s
 
-[[ $displayed = false ]] && printf '%-36s %-14s %-10s %s\n' "$previous_checksum_filename" "$previous_checksum_version" "$previous_checksum_arch" "$previous_checksum_md5"
 
 # sed 's|<?version?>|230101|;s|<?md5?>|bc156789|' package.info
