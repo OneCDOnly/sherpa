c75218cc3b9ec7e27355735e19367e9b72b8e8d9
OneCDOnly <one.cd.only@gmail.com>
2023-02-06 06:10:51 +1000

    refresh QPKG details from source file

--- support/update-package-versions.sh
+++ support/update-package-versions.sh
@@ -41,47 +41,50 @@ rm -f /tmp/raw.md5s
 echo 'extracting highest version numbers ...'
 rm -f /tmp/highest-version.lst
 checksum_pathfilename=''
-checksum_version=''
-checksum_arch=''
+version=''
+arch=''
+md5=''
 previous_checksum_filename=''
 previous_package_name=''
-previous_checksum_version=''
-previous_checksum_arch=''
+previous_version=''
+previous_arch=''
+
 match=false
 
 while read -r checksum_pathfilename; do
     # need just filename
     checksum_filename=$(basename "$checksum_pathfilename")
+    qpkg_filename="${checksum_filename//.md5/}"
 
-    IFS='_' read -r package_name checksum_version checksum_arch tailend <<< "${checksum_filename//.qpkg.md5/}"
+    IFS='_' read -r package_name version arch tailend <<< "${checksum_filename//.qpkg.md5/}"
 
-    if [[ $checksum_arch = std ]]; then     # make an exception for Entware
-        checksum_version+=_$checksum_arch
-        checksum_arch=''
+    if [[ $arch = std ]]; then     # make an exception for Entware
+        version+=_$arch
+        arch=''
         tailend=''
     fi
 
-    [[ -z $checksum_arch ]] && checksum_arch=all
-    [[ -n $tailend ]] && checksum_arch+=_$tailend
+    [[ -z $arch ]] && arch=all
+    [[ -n $tailend ]] && arch+=_$tailend
 
     if [[ $package_name != "$previous_package_name" ]]; then
         match=true
-    elif [[ $checksum_version = "$previous_checksum_version" ]]; then
-        if [[ $checksum_arch != "$previous_checksum_arch" ]]; then
+    elif [[ $version = "$previous_version" ]]; then
+        if [[ $arch != "$previous_arch" ]]; then
             match=true
         fi
     else
         match=false
     fi
 
-    checksum_md5=$(cut -d' ' -f1 < "$checksum_pathfilename")
+    md5=$(cut -d' ' -f1 < "$checksum_pathfilename")
 
     if [[ $match = true ]]; then
-        printf '%-36s %-20s %-14s %-10s %s\n' "$checksum_filename" "$package_name" "$checksum_version" "$(TranslateQPKGArch "$checksum_arch")" "$checksum_md5" >> /tmp/highest-version.lst
+        printf '%-36s %-30s %-20s %-12s %-6s %s\n' "$checksum_filename" "$qpkg_filename" "$package_name" "$version" "$(TranslateQPKGArch "$arch")" "$md5" >> /tmp/highest-version.lst
         previous_checksum_filename=$checksum_filename
         previous_package_name=$package_name
-        previous_checksum_version=$checksum_version
-        previous_checksum_arch=$checksum_arch
+        previous_version=$version
+        previous_arch=$arch
     fi
 done < /tmp/sorted.md5s
 
@@ -91,8 +94,22 @@ echo 'sorting by package name ...'
 sort < /tmp/highest-version.lst > /tmp/final.lst
 rm -f /tmp/highest-version.lst
 
-echo -e "\nfound $(wc -l /tmp/final.lst | cut -d' ' -f1) packages"
-echo '---------- highest package version found -----------'
-printf '%-36s %-20s %-14s %-10s %s\n' filename packagename version arch md5
-cat /tmp/final.lst
-# sed 's|<?version?>|230101|;s|<?md5?>|bc156789|' package.info
+# echo -e "\nfound $(wc -l /tmp/final.lst | cut -d' ' -f1) packages"
+# echo '---------- highest package version found -----------'
+# printf '%-36s %-30s %-20s %-12s %-6s %s\n' checksum_filename qpkg_filename package_name version arch md5
+# cat /tmp/final.lst
+
+echo 'updating package fields ...'
+source=$(<packages.source)
+source=$(sed "s|<?today?>|$(date '+%y%m%d')|" <<< "$source")
+
+while read -r checksum_filename qpkg_filename package_name version arch md5; do
+    source=$(sed "/QPKG_NAME+=($package_name)/,/QPKG_NAME+=/ s/<?version?>/$version/" <<< "$source")
+    source=$(sed "/QPKG_NAME+=($package_name)/,/QPKG_NAME+=/ s/<?package_name?>/$package_name/" <<< "$source")
+    source=$(sed "/QPKG_NAME+=($package_name)/,/QPKG_NAME+=/ s/<?qpkg_filename?>/$qpkg_filename/" <<< "$source")
+    source=$(sed "/QPKG_NAME+=($package_name)/,/QPKG_NAME+=/ s/<?md5?>/$md5/" <<< "$source")
+done < /tmp/final.lst
+
+echo "$source" > packages.final
+
+echo "done."
