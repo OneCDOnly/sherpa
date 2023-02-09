a82ae00f71310927fa72ee6c52bf4cd3bff895c5
OneCDOnly <one.cd.only@gmail.com>
2023-02-09 10:40:20 +1000

    update build scripts

--- support/commit-to-main.sh
+++ support/commit-to-main.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+echo -n "about to commit to 'main' branch: proceed? "
+read -rn1 response
+echo
+
+case ${response:0:1} in
+    y|Y)
+        : # OK to continue
+        ;;
+    *)
+        exit 0
+esac
+
 echo 'main' > "$HOME"/scripts/nas/sherpa/support/branch.txt
 
 ./make.sh || exit
@@ -9,6 +21,7 @@ git add . && git commit -m '[scripted] update archives' && git push
 git checkout main
 git merge unstable && git push
 git checkout unstable
+cd "$HOME"/scripts/nas/sherpa/support || exit
 
 echo 'unstable' > "$HOME"/scripts/nas/sherpa/support/branch.txt
 
--- support/commit-to-unstable.sh
+++ support/commit-to-unstable.sh
@@ -4,5 +4,6 @@ echo 'unstable' > "$HOME"/scripts/nas/sherpa/support/branch.txt
 
 cd "$HOME"/scripts/nas/sherpa || exit
 git add . && git commit && git push
+cd "$HOME"/scripts/nas/sherpa/support || exit
 
 exit 0
