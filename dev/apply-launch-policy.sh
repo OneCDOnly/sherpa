208763c5cac1de72a6de9dbaf8634aac931ba3ae
OneCDOnly <one.cd.only@gmail.com>
2023-03-16 06:30:25 +1000

    update comments

--- dev/apply-launch-policy.sh
+++ dev/apply-launch-policy.sh
@@ -13,45 +13,61 @@
 policy:longest()
 	{
 
+	# package names will be listed with the longest action time first. Packages without times will be listed first.
+
 	[[ -s $action_times_pathfile ]] || return
 	[[ ${#1} -gt 0 ]] || return
 
-# 	local sorted=$(sort -k2 -r <<<"$(for buff in $1; do
-# 		grep "^$buff" "$action_times_pathfile"
-# 	done)" | sed 's/|.*$//' | tr '\n' ' ')
-
-	local package_names=($1)
-	local unsorted_names=()
-	local untimed_names=()
 	local package_name=''
+	local supplied_package_names=($1)
+	local matched_names=()
+	local untimed_names=()
+	local patt=''
 
-# 	DeDupeWords()
-# 		{
-#
-# 		tr ' ' '\n' <<< "${1:-}" | $SORT_CMD | $UNIQ_CMD | tr '\n' ' ' | $SED_CMD 's|^[[:blank:]]*||;s|[[:blank:]]*$||'
-#
-# 		}
+	local sorted=$(sort -k2 -r <<<"$(for package_name in $1; do
+		grep "^$package_name" "$action_times_pathfile"
+	done)" | sed 's/|.*$//' | tr '\n' ' ')
 
-	for package_name in "${package_names[@]}"; do
-		if buff=$(grep "^$package_name" "$action_times_pathfile"); then
-			unsorted_names+=("$buff")
-		else
-			untimed_names+=($package_name)
-		fi
+echo "supplied_package_names[${supplied_package_names[*]}]"
+echo "sorted[$sorted]"
+
+	for sorted_index in "${!sorted[@]}"; do
+echo "checking sorted name [${sorted[$sorted_index]}]"
+
+		patt="\b${sorted_package_name}\b"
+
+		for supplied_package_name in ${supplied_package_names[*]}; do
+echo "checking supplied name [$supplied_package_name]"
+			supp_patt="\b${supplied_package_name}\b"
+
+			if [[ "$sorted" =~ $supp_patt ]]; then
+				echo -e "adding [$sorted_package_name] to matched\n"
+				matched_names+=($sorted_package_name)
+# 			else
+# 				echo "adding [$supplied_package_name] to untimed"
+# 				untimed_names+=($sorted_package_name)
+				break
+			fi
+		done
+
+# 		[[ $sorted_package_name != $supplied_package_name ]] && continue
+#
+# 			if [[ "${supplied_package_names[*]}" =~ $patt ]]; then
+# 				echo "adding [$supplied_package_name] to matched"
+# 				matched_names+=($supplied_package_name)
+# 			else
+# 				echo "adding [$supplied_package_name] to untimed"
+# 				untimed_names+=($supplied_package_name)
+# 			fi
+# 			break
+# 		done
 	done
 
+echo "matched[${matched_names[*]}]"
+echo "untimed[${untimed_names[*]}]"
 
-echo "unsorted_names=[${unsorted_names[@]}]"
-echo "untimed_names=[${untimed_names[@]}]"
-
-
- 	local sorted_names=$(sort -k2 -r <<<"${unsorted_names[@]}")
-echo "sorted_names: [$sorted_names]"
-	local names_only=$(sed 's/|.*$//' <<<"$sorted_names")
-
- 	local untimed=$(sed 's/|.*$//' <<<"$untimed_names" | tr '\n' ' ')
-
-	echo "${untimed%% } ${names_only%% }"
+exit
+	echo "${untimed_names[*]%% } ${matched_names[*]%% }"
 
 	return 0
 
@@ -84,20 +100,29 @@ policy:none()
 
 	} 2>/dev/null
 
-input_names=(c b e g)
+input_names=(c b a e g z d 3)
 
 action=${1:-start}
 echo "action: '$action'"
 
 action_times_pathfile="$action.milliseconds"
 
+policy:longest "${input_names[*]}" || exit
+exit
 sorted_names=$(policy:longest "${input_names[*]}") || exit
 echo "longest: '$sorted_names'"
 
-exit
+# while read -r package_name; do
+# 	echo "newlines name: [$package_name]"
+# done <<< "$(tr ' ' '\n' <<< "$sorted_names")"
 
-sorted_names=$(policy:shortest "${input_names[*]}") || exit
-echo "shortest: '$sorted_names'"
+for package_name in $sorted_names; do
+	echo "spaces name: [$package_name]"
+done
 
-sorted_names=$(policy:none "${input_names[*]}") || exit
-echo "none: '$sorted_names'"
+
+# sorted_names=$(policy:shortest "${input_names[*]}") || exit
+# echo "shortest: '$sorted_names'"
+#
+# sorted_names=$(policy:none "${input_names[*]}") || exit
+# echo "none: '$sorted_names'"
