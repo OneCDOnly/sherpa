4b797462f191f76a697e4edd33a7fe0e1da47e47
OneCDOnly <one.cd.only@gmail.com>
2023-03-19 15:51:12 +1000

    minor tweaks

--- dev/launch-policy.sh
+++ dev/launch-policy.sh
@@ -10,7 +10,7 @@
 # output:
 #	stdout=sorted list of package names
 
-policy:slowest()
+policy:slowest_first()
 	{
 
 	# package names will be listed with the longest action time first. Packages without times will be listed before timed packages.
@@ -25,7 +25,7 @@ policy:slowest()
 	[[ ${#1} -gt 0 ]] || return
 
 	if [[ ! -s $action_times_pathfile ]]; then
-		echo "${1:-}"
+		echo "$1"
 		return
 	fi
 
@@ -39,8 +39,7 @@ policy:slowest()
 
 	# get list of only supplied package names where times have been recorded, sort this list by time, then return names-only
  	local target_timed=$(sort -k2 -r <<<"$(for package in ${target_packages[*]}; do
-		re="\b${package}\b"
-		grep "$re" "$action_times_pathfile"
+		grep "\b${package}\b" "$action_times_pathfile"
 		done )" | sed 's/|.*$//' | tr '\n' ' ')
 
 	# separate timed package names from untimed
@@ -63,13 +62,13 @@ policy:slowest()
 		fi
 	done
 
-	echo "${unsorted_untimed[*]%% } ${sorted_timed[*]%% }"
+	printf "${unsorted_untimed[*]%% } ${sorted_timed[*]%% }"
 
 	return 0
 
 	} 2>/dev/null
 
-policy:quickest()
+policy:quickest_first()
 	{
 
 	# package names will be listed with the shorted action time first. Packages without times will be listed before timed packages.
@@ -84,7 +83,7 @@ policy:quickest()
 	[[ ${#1} -gt 0 ]] || return
 
 	if [[ ! -s $action_times_pathfile ]]; then
-		echo "${1:-}"
+		echo "$1"
 		return
 	fi
 
@@ -98,8 +97,7 @@ policy:quickest()
 
 	# get list of only supplied package names where times have been recorded, sort this list by time, then return names-only
  	local target_timed=$(sort -k2 <<<"$(for package in ${target_packages[*]}; do
-		re="\b${package}\b"
-		grep "$re" "$action_times_pathfile"
+		grep "\b${package}\b" "$action_times_pathfile"
 		done )" | sed 's/|.*$//' | tr '\n' ' ')
 
 	# separate timed package names from untimed
@@ -122,38 +120,34 @@ policy:quickest()
 		fi
 	done
 
-	echo "${unsorted_untimed[*]%% } ${sorted_timed[*]%% }"
+	printf "${unsorted_untimed[*]%% } ${sorted_timed[*]%% }"
 
 	return 0
 
 	} 2>/dev/null
 
-policy:none()
+policy:unsorted()
 	{
 
 	[[ ${#1} -gt 0 ]] || return
 
-	echo "${1%% }"
+	printf "${1%% }"
 
 	return 0
 
 	} 2>/dev/null
 
-input_names=(c ab y d eywtrwu  aa rrr c a)
+input_names=(c ab y e d u 3 aa rrr c a)
 action=${1:-start}
 action_times_pathfile="$action.milliseconds"
 
 echo "action: '$action'"
 
-sorted_names=$(policy:none "${input_names[*]}") || exit
-echo "'none' policy: '$sorted_names'"
+sorted_names=$(policy:unsorted "${input_names[*]}") || exit
+echo "'unsorted' policy: '$sorted_names'"
 
-sorted_names=$(policy:slowest "${input_names[*]}") || exit
-echo "'slowest' policy: '$sorted_names'"
+sorted_names=$(policy:slowest_first "${input_names[*]}") || exit
+echo "'slowest_first' policy: '$sorted_names'"
 
-# for package_name in $sorted_names; do
-# 	echo "whitespace name: '$package_name'"
-# done
-
-sorted_names=$(policy:quickest "${input_names[*]}") || exit
-echo "'quickest' policy: '$sorted_names'"
+sorted_names=$(policy:quickest_first "${input_names[*]}") || exit
+echo "'quickest_first' policy: '$sorted_names'"
