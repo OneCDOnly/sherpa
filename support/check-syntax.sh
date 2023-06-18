6cc03cbdcc213bee1240db7ed2c88acd24788583
OneCDOnly <one.cd.only@gmail.com>
2023-06-19 07:43:33 +1000

    update to run on Debian Bookworm

--- support/check-syntax.sh
+++ support/check-syntax.sh
@@ -15,10 +15,10 @@ filenames+=(sherpa.manager.source)
 exclusions+=(1090,1117,2012,2015,2016,2018,2019,2034,2086,2119,2120,2128,2155,2181,2206,2207)
 
 filenames+=(packages.source)
-exclusions+=(1036,1088,2034)
+exclusions+=(1009,1036,1072,1073,1088,2034)
 
 filenames+=('*.sh')
-exclusions+=(1036,1090,1091,2001,2006,2016,2034,2054,2086,2154,2155)
+exclusions+=(1036,1090,1091,2001,2006,2016,2028,2034,2054,2086,2154,2155)
 
 for index in "${!filenames[@]}"; do
 	echo -n "checking '${filenames[index]}' ... "
