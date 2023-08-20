31da12551bea858b80fe988a6a68fbd970efcab2
OneCDOnly <one.cd.only@gmail.com>
2023-08-20 14:42:25 +1000

    include packages built with auto-build system - also, begin rollout of service script function library.

--- support/check-syntax.sh
+++ support/check-syntax.sh
@@ -14,8 +14,13 @@ declare -i index=0
 filenames+=(sherpa.manager.source)
 exclusions+=(1090,1117,2012,2015,2016,2018,2019,2034,2086,2119,2120,2128,2155,2181,2194,2206,2207,2254)
 
+filenames+=(service-library.source)
+exclusions+=(1087,1090,1117,2012,2015,2016,2018,2019,2034,2086,2119,2120,2128,2155,2181,2194,2206,2207,2254)
+
 filenames+=(packages.source)
 exclusions+=(1009,1036,1072,1073,1088,2034)
+
+rm -f service-library.sh		# don't check this
 
 filenames+=('*.sh')
 exclusions+=(1036,1090,1091,2001,2006,2016,2028,2034,2054,2086,2154,2155)
