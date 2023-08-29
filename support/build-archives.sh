2ceb7268356f28608216a5440355935b8bd53680
OneCDOnly <one.cd.only@gmail.com>
2023-08-30 06:53:14 +1000

    use global vars for filenames built using automation

--- support/build-archives.sh
+++ support/build-archives.sh
@@ -15,14 +15,14 @@ declare -a source_pathfiles
 declare -a target_pathfiles
 declare -i index=0
 
-source_pathfiles+=("$source_path"/objects)
-target_pathfiles+=("$target_path"/objects.tar.gz)
+source_pathfiles+=("$source_path/$objects_file")
+target_pathfiles+=("$target_path/$objects_archive_file")
 
-source_pathfiles+=("$source_path"/packages)
-target_pathfiles+=("$target_path"/packages.tar.gz)
+source_pathfiles+=("$source_path/$packages_file")
+target_pathfiles+=("$target_path/$packages_archive_file")
 
-source_pathfiles+=("$source_path"/sherpa-manager.sh)
-target_pathfiles+=("$target_path"/sherpa-manager.tar.gz)
+source_pathfiles+=("$source_path/$management_file")
+target_pathfiles+=("$target_path/$management_archive_file")
 
 for index in "${!source_pathfiles[@]}"; do
 	[[ -e ${target_pathfiles[index]} ]] && rm -f "${target_pathfiles[index]}"
--- support/build-manager.sh
+++ support/build-manager.sh
@@ -9,8 +9,8 @@ fi
 
 echo -n 'building management script ... '
 
-source_pathfile="$source_path"/sherpa-manager.source
-target_pathfile="$source_path"/sherpa-manager.sh
+source_pathfile="$source_path/$management_source_file"
+target_pathfile="$source_path/$management_file"
 
 buffer=$(<"$source_pathfile")
 
