7f590842340a9914bf11f76a750de9c231956a9e
OneCDOnly <one.cd.only@gmail.com>
2023-09-06 10:44:18 +1000

    switch LazyLibrarian to use service function library

---
+++ support/list-objects-inits.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [[ ! -e vars.source ]]; then
+	echo "'vars.source' not found"
+	exit 1
+fi
+
+. ./vars.source
+
+source_pathfile="$source_path/$objects_file"
+
+[[ ! -e $source_pathfile ]] && ./build-objects.sh
+
+grep '.Init()' "$source_pathfile" | sort
