dfe5dae1769575e2467878c8c77ffe71283ebe67
OneCDOnly <one.cd.only@gmail.com>
2023-09-10 10:51:00 +1000

    update comments

--- support/list-functions-unused.sh
+++ support/list-functions-unused.sh
@@ -13,10 +13,10 @@ target_func=''
 # shellcheck disable=SC2013
 for target_func in $(grep '()$' "$source_pathfile" | grep -v '=\|\$\|_(' | sed 's|()||g'); do
 	case $target_func in
-		IPKs:upgrade|IPKs:install|IPKs:downgrade|PIPs:install)		# called by constructing the function name with vars
+		IPKs:upgrade|IPKs:install|IPKs:downgrade|PIPs:install)		# called by constructing the function name with vars.
 			continue
 			;;
-		QPKGs.Actions:ListAll|QPKG.GetAppAuthor)	# unused for-now
+		QPKGs.Actions:ListAll|QPKG.GetAppAuthor)	# unused for-now.
 			continue
 	esac
 
