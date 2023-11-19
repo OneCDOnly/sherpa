f3dd72b517aeb68ca34e73a7b02e01269699d49f
OneCDOnly <one.cd.only@gmail.com>
2023-11-19 16:05:07 +1000

    update build process

--- support/build-readme.sh
+++ support/build-readme.sh
@@ -10,7 +10,7 @@ fi
 [[ ! -e $highest_package_versions_found_sorted_pathfile ]] && ./build-packages.sh "${1:-$unstable_branch}"
 
 latest_release_version=$(grep ^sherpa_ "$highest_package_versions_found_sorted_pathfile" | tr -s ' ' | cut -d' ' -f4)
-echo "latest_release_version: [$latest_release_version]"
+echo "latest release version: $latest_release_version"
 
 source_pathfile=$docs_path/$readme_source_file
 target_pathfile=$docs_path/$readme_file
