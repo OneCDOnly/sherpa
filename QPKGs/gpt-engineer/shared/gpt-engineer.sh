d16c33daac320226e43644d2e132f5e0c070231b
OneCDOnly <one.cd.only@gmail.com>
2024-01-07 09:38:30 +1000

    fix dependency required for Glances - and build an initial gpt-engineer QPKG

---
+++ QPKGs/gpt-engineer/shared/gpt-engineer.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#* don't edit this file, it was built/modified programmatically with the `build-qpkgs.sh` script. (source: gpt-engineer.source)
+#* gpt-engineer.sh
+#* Copyright (C) 2017-2024 OneCD - one.cd.only@gmail.com
+#*   So, blame OneCD if it all goes horribly wrong. ;)
+#* Project:
+#*	 https://git.io/sherpa
+#* Forum:
+#*	 https://forum.qnap.com/viewtopic.php?f=320&t=132373
+#* Tested on:
+#*	 GNU bash, version 3.2.57(2)-release (i686-pc-linux-gnu)
+#*	 GNU bash, version 3.2.57(1)-release (aarch64-QNAP-linux-gnu)
+#*	   Copyright (C) 2007 Free Software Foundation, Inc.
+#* ... and periodically on:
+#*	 GNU bash, version 5.0.17(1)-release (aarch64-openwrt-linux-gnu)
+#*	   Copyright (C) 2019 Free Software Foundation, Inc.
+#* License:
+#*   This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#*	 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#*	 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/
+readonly USER_ARGS_RAW=$*
+readonly QPKG_NAME=gpt-engineer
+readonly SERVICE_SCRIPT_VERSION='240107'
+InitBasic()
+{
+service_script_type=7
+}
+InitComplex()
+{
+daemon_pathfile=$venv_path/bin/gpte
+daemon_launch_cmd="$venv_python_pathfile $daemon_pathfile --webserver"
+}
+library_path=$(/usr/bin/readlink "$0" 2>/dev/null)
+[[ -z $library_path ]] && library_path=$0
+readonly SERVICE_LIBRARY_PATHFILE=$(/usr/bin/dirname "$library_path")/service.lib
+if [[ -e $SERVICE_LIBRARY_PATHFILE ]]; then
+. $SERVICE_LIBRARY_PATHFILE
+else
+printf '\033[1;31m%s\033[0m: %s\n' 'derp' "QPKG service function library not found, can't continue."
+exit 1
+fi
+ProcessArgs
