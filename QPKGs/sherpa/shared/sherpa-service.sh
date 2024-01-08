d1ff3b570b16db9f65f0ffc1e16fade7cda79ca5
OneCDOnly <one.cd.only@gmail.com>
2024-01-09 06:07:37 +1000

    build separate SABnzbd QPKGs for each arch

--- QPKGs/sherpa/shared/sherpa-service.sh
+++ QPKGs/sherpa/shared/sherpa-service.sh
@@ -24,9 +24,9 @@ Init()
 {
 local -r QPKG_PATH=$(/sbin/getcfg sherpa Install_Path -f /etc/config/qpkg.conf)
 readonly REAL_LOG_PATHFILE=$QPKG_PATH/logs/session.archive.log
-readonly GUI_LOG_PATHFILE=/home/httpd/sherpa.debug.log
 readonly REAL_LOADER_SCRIPT_PATHNAME=$QPKG_PATH/sherpa-loader.sh
 readonly APPARENT_LOADER_SCRIPT_PATHNAME=/usr/sbin/sherpa
+readonly GUI_LOG_PATHFILE=/home/httpd/sherpa.debug.log
 readonly SERVICE_STATUS_PATHFILE=/var/run/sherpa.last.operation
 [[ ! -d $(/usr/bin/dirname "$REAL_LOG_PATHFILE") ]] && mkdir -p "$(/usr/bin/dirname "$REAL_LOG_PATHFILE")"
 [[ ! -e $REAL_LOG_PATHFILE ]] && /bin/touch "$REAL_LOG_PATHFILE"
