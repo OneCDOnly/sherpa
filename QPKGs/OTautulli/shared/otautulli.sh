e2c3e4ae2206e5787cf6cff575c6d9fca9d0759d
OneCDOnly <one.cd.only@gmail.com>
2024-01-16 08:09:49 +1000

    fix port conflict between Tautulli and Headphones - Headphones already uses 8181, so switch Tautulli to use 4141.

--- QPKGs/OTautulli/shared/otautulli.sh
+++ QPKGs/OTautulli/shared/otautulli.sh
@@ -20,15 +20,15 @@
 #*	 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/
 readonly USER_ARGS_RAW=$*
 readonly QPKG_NAME=OTautulli
-readonly SERVICE_SCRIPT_VERSION='240115'
+readonly SERVICE_SCRIPT_VERSION='240116'
 readonly SERVICE_SCRIPT_TYPE=1
 InitService()
 {
 daemon_pathfile=$qpkg_repo_path/Tautulli.py
 daemon_launch_cmd="$venv_python_pathfile $daemon_pathfile --daemon --nolaunch --datadir $(/usr/bin/dirname "$qpkg_ini_pathfile") --config $qpkg_ini_pathfile --pidfile $daemon_pid_pathfile"
 get_ui_listening_address_cmd="/sbin/getcfg misc host -d 0.0.0.0 -f $qpkg_ini_pathfile"
-get_ui_port_cmd="/sbin/getcfg General http_port -d 8181 -f $qpkg_ini_pathfile"
-get_ui_port_secure_cmd="/sbin/getcfg General http_port -d 8181 -f $qpkg_ini_pathfile"
+get_ui_port_cmd="/sbin/getcfg General http_port -d 4141 -f $qpkg_ini_pathfile"
+get_ui_port_secure_cmd="/sbin/getcfg General http_port -d 4141 -f $qpkg_ini_pathfile"
 get_ui_port_secure_enabled_test_cmd='[[ $(/sbin/getcfg General https_enabled -d 0 -f '$qpkg_ini_pathfile') = 1 ]]'
 source_git_branch=master
 source_git_url=https://github.com/Tautulli/Tautulli.git
