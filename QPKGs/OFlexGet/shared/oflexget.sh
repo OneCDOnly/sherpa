981e7fb1a835f3a088309ff3c208f6451dd3b92b
OneCDOnly <one.cd.only@gmail.com>
2024-01-17 05:00:27 +1000

    initial push of FlexGet QPKG

---
+++ QPKGs/OFlexGet/shared/oflexget.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#* Please don't edit this file directly, it was built/modified programmatically with the 'build-qpkgs.sh' script. (source: 'oflexget.source')
+#* oflexget.sh
+#* Copyright (C) 2017-2024 OneCD - one.cd.only@gmail.com
+#*   So, blame OneCD if it all goes horribly wrong. ;)
+#* Project:
+#*	 https://git.io/sherpa
+#* Forum:
+#*	 https://forum.qnap.com/viewtopic.php?f=320&t=132373
+#* Tested on:
+#*	 GNU bash, version 3.2.57(2)-release (i686-pc-linux-gnu)
+#*	 GNU bash, version 3.2.57(1)-release (aarch64-QNAP-linux-gnu)
+#*	   Copyright (C) 2007 Free Software Foundation, Inc.
+#* ... and periodically on:
+#*	 GNU bash, version 5.0.17(1)-release (aarch64-openwrt-linux-gnu)
+#*	   Copyright (C) 2019 Free Software Foundation, Inc.
+#* License:
+#*   This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#*	 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#*	 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/
+readonly USER_ARGS_RAW=$*
+readonly QPKG_NAME=OFlexGet
+readonly SERVICE_SCRIPT_VERSION='240117'
+readonly SERVICE_SCRIPT_TYPE=6
+InitService()
+{
+:
+daemon_pathfile=$venv_path/bin/flexget
+daemon_launch_cmd="$venv_python_pathfile $daemon_pathfile"
+}
+library_path=$(/usr/bin/readlink "$0" 2>/dev/null)
+[[ -z $library_path ]] && library_path=$0
+readonly SERVICE_LIBRARY_PATHFILE=$(/usr/bin/dirname "$library_path")/service.lib
+if [[ -e $SERVICE_LIBRARY_PATHFILE ]]; then
+. $SERVICE_LIBRARY_PATHFILE
+else
+printf '\033[1;31m%s\033[0m: %s\n' 'derp' "QPKG service function library not found, can't continue."
+exit 1
+fi
+ProcessArgs
