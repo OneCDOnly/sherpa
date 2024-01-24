241325c39133886badb937dbb0d9cefcf815c3d5
OneCDOnly <one.cd.only@gmail.com>
2024-01-25 05:42:34 +1000

    [update] archives

--- QPKGs/OqBittorrent/shared/oqbittorrent.sh
+++ QPKGs/OqBittorrent/shared/oqbittorrent.sh
@@ -20,7 +20,7 @@
 #*	 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/
 readonly USER_ARGS_RAW=$*
 readonly QPKG_NAME=OqBittorrent
-readonly SERVICE_SCRIPT_VERSION='240124'
+readonly SERVICE_SCRIPT_VERSION='240125'
 readonly SERVICE_SCRIPT_TYPE=3
 InitService()
 {
