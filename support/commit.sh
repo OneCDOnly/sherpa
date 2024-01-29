0aa0fbeeb60c84ea2bc36fe554559435e55951f5
OneCDOnly <one.cd.only@gmail.com>
2024-01-30 06:28:10 +1000

    fix a few shellcheck pickups

--- support/commit.sh
+++ support/commit.sh
@@ -3,7 +3,10 @@
 source_path="$HOME"/scripts/nas/sherpa/support
 . $source_path/vars.source || exit
 
+./check-syntax.sh || exit
+
 [[ -e $objects_file ]] && rm -f "$objects_file"
+
 cd "$target_path" || exit
 git add . && git commit && git push
 cd "$source_path" || exit
