0b6b0f2af4d298f784eec5a37e598cfa7b5bfe42
OneCDOnly <one.cd.only@gmail.com>
2024-01-31 09:35:43 +1000

    [update] workshop

--- support/commit.sh
+++ support/commit.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
 source_path="$HOME"/scripts/nas/sherpa/support
+this_path=$PWD
 . $source_path/vars.source || exit
 
+cd "$source_path" || exit
 ./check-syntax.sh || exit
 
 [[ -e $objects_file ]] && rm -f "$objects_file"
+[[ -e $management_file ]] && rm -f "$management_file"
+[[ -e $packages_file ]] && rm -f "$packages_file"
 
 cd "$target_path" || exit
+
 git add . && git commit && git push
-cd "$source_path" || exit
+
+cd "$this_path" || exit
 
 exit 0
