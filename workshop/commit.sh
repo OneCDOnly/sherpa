c3af9ac04cfc0dca27d793f389d6115b69bcca24
OneCDOnly <one.cd.only@gmail.com>
2024-01-31 09:35:43 +1000

    [update] workshop

--- workshop/commit.sh
+++ workshop/commit.sh
@@ -1,1 +1,19 @@
-../support/commit.sh
+#!/usr/bin/env bash
+
+source_path="$HOME"/scripts/nas/sherpa/support
+this_path=$PWD
+. $source_path/vars.source || exit
+
+cd "$source_path" || exit
+
+[[ -e $objects_file ]] && rm -f "$objects_file"
+[[ -e $management_file ]] && rm -f "$management_file"
+[[ -e $packages_file ]] && rm -f "$packages_file"
+
+cd "$target_path" || exit
+
+git add . && git commit -m '[update] workshop' && git push
+
+cd "$this_path" || exit
+
+exit 0
