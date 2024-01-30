8626e255acdf7a6a2cb3b8d84779c13ac9b510da
OneCDOnly <one.cd.only@gmail.com>
2024-01-30 12:19:58 +1000

    [update] archives

--- QPKGs/Headphones/shared/headphones.sh
+++ QPKGs/Headphones/shared/headphones.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 #* Please don't edit this file directly, it was built/modified programmatically with the 'build-qpkgs.sh' script. (source: 'headphones.source')
 #* headphones.sh
-#* Copyright (C) 2017-2024 OneCD - one.cd.only@gmail.com
-#*   So, blame OneCD if it all goes horribly wrong. ;)
+#* Copyright (C) 2017-2024 OneCD.
+#* Contact:
+#*   one.cd.only@gmail.com
 #* Project:
 #*	 https://git.io/sherpa
 #* Forum:
@@ -11,7 +12,7 @@
 #*	 GNU bash, version 3.2.57(2)-release (i686-pc-linux-gnu)
 #*	 GNU bash, version 3.2.57(1)-release (aarch64-QNAP-linux-gnu)
 #*	   Copyright (C) 2007 Free Software Foundation, Inc.
-#* ... and periodically on:
+#*   ... and periodically on:
 #*	 GNU bash, version 5.0.17(1)-release (aarch64-openwrt-linux-gnu)
 #*	   Copyright (C) 2019 Free Software Foundation, Inc.
 #* License:
