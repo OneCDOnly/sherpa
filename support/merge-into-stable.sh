0908eb4e7951b5c7adb87146015cf3c57092769c
OneCDOnly <one.cd.only@gmail.com>
2024-02-03 11:55:32 +1000

    update commit-and-merge logic when releasing new sherpa version

--- support/merge-into-stable.sh
+++ support/merge-into-stable.sh
@@ -14,12 +14,18 @@ case ${response:0:1} in
 		exit 0
 esac
 
+cd $HOME/scripts/nas/sherpa/support || exit
+./update-archives.sh || exit
+
 cd $HOME/scripts/nas/sherpa || exit
 
 git checkout "$stable_branch" || exit
-git merge --no-ff -m "[merge] from \`$unstable_branch\` into \`$stable_branch\`" "$unstable_branch" && git push
+git merge --no-ff -m "[merge] from \`$unstable_branch\` into \`$stable_branch\`" "$unstable_branch" && git push || exit
 git checkout "$unstable_branch" || exit
 
 cd $HOME/scripts/nas/sherpa/support || exit
+
+./build-qpkgs.sh || exit
+./commit.sh '[rebuild] post-merge' || exit
 
 exit 0
--- support/prepare-for-release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# touch a file in the main sherpa QPKG so it will be rebuilt by 'build-qpkgs.sh'
-
-. vars.source || exit
-
-touch "$qpkgs_path"/sherpa/qpkg.source
