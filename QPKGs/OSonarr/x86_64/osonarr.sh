8b25fffc70d8e3009b25de719c3ff168aac48cff
OneCDOnly <one.cd.only@gmail.com>
2024-02-08 11:01:10 +1000

    [rebuild] post-merge

--- QPKGs/OSonarr/x86_64/osonarr.sh
+++ QPKGs/OSonarr/x86_64/osonarr.sh
@@ -21,7 +21,7 @@
 #*	 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/
 readonly USER_ARGS_RAW=$*
 readonly QPKG_NAME=OSonarr
-readonly SERVICE_SCRIPT_VERSION='240206'
+readonly SERVICE_SCRIPT_VERSION='240208'
 readonly SERVICE_SCRIPT_TYPE=5
 InitService()
 {
