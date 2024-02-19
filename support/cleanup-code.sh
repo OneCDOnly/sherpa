908dee4dcfc807d30de868908cc975d53ad1b419
OneCDOnly <one.cd.only@gmail.com>
2024-02-20 07:24:08 +1000

    check `service-library.source` modification time, not change time - change time is altered when cleaning `service-library.source`, so don't examine this.

--- support/cleanup-code.sh
+++ support/cleanup-code.sh
@@ -6,12 +6,15 @@ declare -a filenames
 declare -i index=0
 
 filenames+=("$management_source_file")
-# filenames+=("$service_library_source_file")
+filenames+=("$service_library_source_file")
 
 for index in "${!filenames[@]}"; do
 	echo -n "cleaning code '${filenames[index]}' ... "
 
+	touch --reference="$source_path"/"${filenames[index]}" /tmp/"$index".tmp
 	sed -i 's|^[ ][\t]|\t|' "$source_path"/${filenames[index]}		# remove leading space char left by kate line commenter/uncommenter
+	touch --reference=/tmp/"$index".tmp "$source_path"/"${filenames[index]}"
+
 	ShowPassed
 done
 
