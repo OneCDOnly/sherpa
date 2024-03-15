78ee95b85baaa02cabaf5786cdad53ed3f687bed
OneCDOnly <one.cd.only@gmail.com>
2024-03-16 07:07:25 +1000

    [update] archives [pre-merge]

--- QPKGs/sherpa/shared/sherpa-loader.sh
+++ QPKGs/sherpa/shared/sherpa-loader.sh
@@ -24,7 +24,7 @@
 readonly USER_ARGS_RAW=$*
 Init()
 {
-export LOADER_SCRIPT_VER='240307'
+export LOADER_SCRIPT_VER='240316'
 export LOADER_SCRIPT_PPID=$PPID
 readonly QPKG_NAME=sherpa
 readonly CHARS_REGULAR_PROMPT='$ '
