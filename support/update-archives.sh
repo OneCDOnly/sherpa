a31c613538ed0fde97fb37e7ac3b1d51b2719796
OneCDOnly <one.cd.only@gmail.com>
2024-05-01 15:12:30 +1000

    remove refs to QPKG building

--- support/update-archives.sh
+++ support/update-archives.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-this_path=$PWD
-. $HOME/scripts/nas/sherpa/support/vars.source || exit
-
-cd "$support_path" || exit
+. vars.source || exit
 
 ./build-all.sh || exit
 ./commit.sh '[update] management archives' || exit
-
-cd "$qpkgs_root_path" || exit
-git add .
-git commit -m '[update] QPKG archives' || exit
-git push
-
-cd "$this_path" || exit
