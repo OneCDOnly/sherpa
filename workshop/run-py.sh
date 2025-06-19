9daed874719d707f55d5085c1f630e7bc02139cf
OneCDOnly <one.cd.only@gmail.com>
2025-06-19 17:08:46 +1000

    use Python2 code for QPKG signing management - Python2 is included with QTS and sqlite3 module works without requiring additional dependencies to be installed.

--- workshop/run-py.sh
+++ workshop/run-py.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-certificate_db_pathfile=nas_sign_qpkg.db
-qpkg_name=RunLast
+certificate_db_pathfile=/etc/config/nas_sign_qpkg.db
+# qpkg_name=RusnLast
+qpkg_name=helpdesk
 
-read -r -d '' a <<EOB
+a="SELECT 1 from Certificate WHERE QpkgName = '$qpkg_name'"
+b=''
+
+read -r -d '' b <<EOB
 import sqlite3, sys
 
 try:
     sqliteConnection = sqlite3.connect('$certificate_db_pathfile')
 
     cursor = sqliteConnection.cursor()
-    sqlite_select_query = """SELECT 1 from Certificate WHERE QpkgName = '$qpkg_name'"""
+    sqlite_select_query = """$a"""
     cursor.execute(sqlite_select_query)
     records = cursor.fetchall()
     sys.exit(len(records) == 0)
@@ -21,4 +25,4 @@ finally:
         sqliteConnection.close()
 EOB
 
-python2 -c "$a"
+/usr/local/bin/python2 -c "$b"
