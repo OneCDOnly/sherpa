89628ae67171dd22c9ecd99e0bc7297cf9fc5a3d
OneCDOnly <one.cd.only@gmail.com>
2025-07-13 14:53:41 +1000

    update code cleaner

--- support/clean-source.sh
+++ support/clean-source.sh
@@ -12,6 +12,7 @@ for i in "${!a[@]}"; do
 
 	touch --reference="$support_path"/"${a[i]}" /tmp/"$i".tmp
 	sed -i 's|^[ ][\t]|\t|' "$support_path"/${a[i]}					# remove leading space char left by Kate line commenter/uncommenter
+	sed -i '/./b;:n;N;s/\n$//;tn' "$support_path"/${a[i]}   		# squeeze multiple linespaces to a single linespace https://unix.stackexchange.com/a/131228/94864
 	touch --reference=/tmp/"$i".tmp "$support_path"/"${a[i]}"
 
 	ShowDone
